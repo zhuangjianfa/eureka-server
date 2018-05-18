17cbee4b447e39c53861df44dd46e3efd1728249
zhuangjianfa <zhuangjianfa@192.168.1.153>
2018-05-18 09:43:56 +0800

    eureka服务注册中心

---
+++ src/main/java/com/owner/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.owner.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * description:eureka注册中心
+ * Author	Date	Changes
+ * zhuangjianfa  2018年4月26日 Created
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
